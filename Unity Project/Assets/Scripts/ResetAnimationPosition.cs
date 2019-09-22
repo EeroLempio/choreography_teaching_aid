189537c839e5ac4560a2e124f77cf49a4039b8fd
Eero <eerolempio@gmail.com>
2019-09-23 01:04:25 +0300

    Initial animation transition script

---
+++ Unity Project/Assets/Scripts/ResetAnimationPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetAnimationPosition : MonoBehaviour
+{
+    public Transform pointOfReference;
+    Vector3 offSetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offSetPos = pointOfReference.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setPosition()
+    {
+        transform.position += pointOfReference.position - offSetPos;
+        pointOfReference.position = offSetPos;
+    }
+}
