8358e545b8da71af321db4b28d8abe9f19b3b9f7
ilari242 <ilari242@hotmail.com>
2019-10-22 16:33:32 +0300

    Animation aditing

--- Unity Project/Assets/Scripts/DropdownHandler.cs
+++ Unity Project/Assets/Scripts/DropdownHandler.cs
@@ -15,19 +15,61 @@ public class DropdownHandler : MonoBehaviour
         switch (myDropdown.value)
         {
             case 1:
-                myFigure = "NatTF(1)";
+                myFigure = "Natural_Closed_change_123";
                 break;
             case 2:
-                myFigure = "SpinT(1)";
+                myFigure = "Natural_Closed_Change_456";
                 break;
             case 3:
-                myFigure = "RevTB(1)";
+                myFigure = "Natural_Turn_123";
                 break;
             case 4:
-                myFigure = "RevTF(1)";
+                myFigure = "Natural_Turn_456";
                 break;
             case 5:
-                myFigure = "BasicLF(1)";
+                myFigure = "Natural_Turn_Outside_Partner";
+                break;
+            case 6:
+                myFigure = "Open_Impetus";
+                break;
+            case 7:
+                myFigure = "Open_Telemark";
+                break;
+            case 8:
+                myFigure = "Weave_From_PP";
+                break;
+            case 9:
+                myFigure = "Whisk";
+                break;
+            case 10:
+                myFigure = "Chasse_From_PP";
+                break;
+            case 11:
+                myFigure = "Chasse_To_Right";
+                break;
+            case 12:
+                myFigure = "Closed_Telemark";
+                break;
+            case 13:
+                myFigure = "Hesitation";
+                break;
+            case 14:
+                myFigure = "Outside_Change";
+                break;
+            case 15:
+                myFigure = "Reverse_Closed_Change_123";
+                break;
+            case 16:
+                myFigure = "Reverse_Closed_Change_456";
+                break;
+            case 17:
+                myFigure = "Reverse_Turn_123";
+                break;
+            case 18:
+                myFigure = "Reverse_Turn_456";
+                break;
+            case 19:
+                myFigure = "Spin_Turn_Under_Turned";
                 break;
         }
     }
